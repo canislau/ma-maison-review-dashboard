a4e9801fa83b894d11cd7a202af4f4a42065b206
canislau <canislau@theclearwatergroup.com>
2026-07-15 11:29:06 +0800

    0a8f183 Replace broken upload with Google keyless dashboard

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { GoogleAuthProvider } from "./lib/googleAuth";
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode><GoogleAuthProvider><App /></GoogleAuthProvider></React.StrictMode>
-);
